7408e3b89ab7311adaba552fd8cddffa1252f74c
MarcoPerta <perta.marco@gmail.com>
2022-11-08 15:10:28 +0100

    begin new snacks

---
+++ snack3.js
@@ -0,0 +1,20 @@
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+let bike = document.getElementById('bike')
+
+const bici = [
+    {
+      nome: 'Atala',
+      peso: 60,
+    },
+    {
+      nome: 'Bianchi',
+      peso: 70,
+    }
+  ]
+
+  let bikes = bici.filter( ( {nome, peso} )=>{
+
+    if
+  
+  } )
